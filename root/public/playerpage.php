a7c3625f0b102b43b1c569631eb2c12a15a5c066
JLont <d228214@edu.rocwb.nl>
2017-06-01 15:43:20 +0200

    Update playerpage
    Update playerpage

--- root/public/playerpage.php
+++ root/public/playerpage.php
@@ -1,9 +1,19 @@
 <?php
 require_once(__DIR__."/../app/players/fetchPlayer.php");
+include_once("tamplates/header.php");
 
-    if(isset($_SESSION["playerId"])){
-        $player = fetchPlayer($_SESSION["playerId"]);
+    echo "<div class=\"wrapper wrapper_page\">";
 
-        echo $player;
-    }
+        if(isset($_SESSION["playerId"])){
+            $player = fetchPlayer($_SESSION["playerId"]);
+            var_dump($player);
 
+            echo "<h2>Player name:".$player[0]['name']."</h2>";
+
+        }
+        else{
+            echo "<h3>You are not a player.</h3>";
+        }
+include_once("tamplates/footer.php");
+echo "</div>";
+?>
